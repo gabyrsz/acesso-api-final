17ae7b4e949aebc7b8789f6335029a97101b24b0
gabyrsz <gabriellerossi.ifmg@gmail.com>
2025-04-29 21:16:57 -0300

    Registrando services e instalando libs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,58 @@
+using api_acesso_ia;
+using api_acesso_ia.Models;
+using api_acesso_ia.Repositories;
+using api_acesso_ia.Repositories.Interfaces;
+using api_acesso_ia.Services;
+using api_acesso_ia.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// CORS
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:8080") 
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
+
+builder.Services.Configure<AwsSettings>(builder.Configuration.GetSection("AWS"));
+
+
+//banco
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(op =>
+    op.UseMySql(connectionString, ServerVersion.Parse("5.7.0"))
+);
+
+
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+builder.Services.AddScoped<ILoginService, LoginService>();
+builder.Services.AddScoped<ILoginRepository, LoginRepository>();
+
+builder.Services.AddScoped<IAcessoService, AcessoService>();
+builder.Services.AddScoped<IAcessoRepository, AcessoRepository>();
+
+
+builder.Services.AddScoped<IImagemService, ImagemService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,6 +60,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
